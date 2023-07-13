e3f04a2bc6a85dd65880fed74260835ff88d177e
jonybotto <joao_b96@outlook.com>
2023-07-13 09:25:07 -0400

    update

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -5,9 +5,9 @@ const Currency = () => {
     const {dispatch} = useContext(AppContext)
 
     return (
-        <div className='badge-success'>
-            <span>Currency:
-                <select id='currency' onChange={(event) => {dispatch({type: 'CHG_CURRENCY', payload: event.target.value})}}>
+        <div className='currency' style={{backgroundColor: 'lightgreen', color: 'white', padding: '5px', borderRadius: '7px', display: 'flex', justifyContent: 'flex-end', marginLeft: '127px'}}>
+            <span>{'Currency: '}
+                <select id='currency' onChange={(event) => {dispatch({type: 'CHG_CURRENCY', payload: event.target.value})}} style={{backgroundColor: 'lightgreen', color: 'white', borderColor: 'lightgreen'}}>
                     <option value='£' name='£ Pound'>£ Pound</option>
                     <option value='€' name='€ Euro'>€ Euro</option>
                     <option value='$' name='$ Dollar'>$ Dollar</option>
