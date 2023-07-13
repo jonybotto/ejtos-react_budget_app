0a85460ce7f9c57c68e6e3707ca67774e239dfcc
jonybotto <joao_b96@outlook.com>
2023-07-13 14:44:31 -0400

    Final Project

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -8,10 +8,10 @@ const Currency = () => {
         <div className='currency' style={{backgroundColor: 'lightgreen', color: 'white', padding: '5px', borderRadius: '7px', display: 'flex', justifyContent: 'flex-end', marginLeft: '127px'}}>
             <span>{'Currency: '}
                 <select id='currency' onChange={(event) => {dispatch({type: 'CHG_CURRENCY', payload: event.target.value})}} style={{backgroundColor: 'lightgreen', color: 'white', borderColor: 'lightgreen'}}>
-                    <option value='£' name='£ Pound'>£ Pound</option>
-                    <option value='€' name='€ Euro'>€ Euro</option>
-                    <option value='$' name='$ Dollar'>$ Dollar</option>
-                    <option value='₹' name='₹ Rupee'>₹ Rupee</option>
+                    <option value='£' name='£ Pound' style={{color: 'black'}}>£ Pound</option>
+                    <option value='€' name='€ Euro' style={{color: 'black'}}>€ Euro</option>
+                    <option value='$' name='$ Dollar' style={{color: 'black'}}>$ Dollar</option>
+                    <option value='₹' name='₹ Rupee' style={{color: 'black'}}>₹ Rupee</option>
                 </select>
             </span>
         </div>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -15,7 +15,7 @@ const ExpenseItem = (props) => {
     const increaseAllocation = (name) => {
         const expense = {
             name: name,
-            cost: 10,
+            cost: 10
         };
         dispatch({
             type: 'ADD_EXPENSE',
@@ -23,11 +23,23 @@ const ExpenseItem = (props) => {
         });
     }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10
+        }
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        })
+    }
+
     return (
         <tr>
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><button onClick={event=> increaseAllocation(props.name)} style={{backgroundColor: 'rgb(0, 200, 0)', color: 'white', border: 'none', borderRadius: '50%', width: '2rem', height: '2rem', padding: '0', margin: '0', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: 30, fontWeight: 'bold', textAlign: 'center'}}>+</button></td>
+        <td><button onClick={event=> decreaseAllocation(props.name)} style={{backgroundColor: 'rgb(200, 0, 0)', color: 'white', border: 'none', borderRadius: '50%', width: '2rem', height: '2rem', padding: '0', margin: '0', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: 30, fontWeight: 'bold', textAlign: 'center'}}>-</button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
